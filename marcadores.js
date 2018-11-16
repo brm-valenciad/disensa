9456477e70aad3edd6b8db6ababe722d09461532
brm-valenciad <42819769+brm-valenciad@users.noreply.github.com>
2018-11-16 16:17:13 -0500

    UPDATE

---
+++ marcadores.js
@@ -0,0 +1,5 @@
+$("#item-articulo").find(".text-black-50.p-0 m-0.p-1").each(function(){
+	console.info($(this).css("border","1px solid red"));
+});
+
+console.info(":::Los marcadores estan funcionando:::");
